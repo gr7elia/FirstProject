266da1ee7e7592c4bfd04b5387649285480d14b8
gr7elia <zeitahma@kkg-do.eu>
2024-09-23 08:36:32 +0200

    Initialer Commit für Repository-Start

---
+++ src/MainControl.java
@@ -0,0 +1,20 @@
+public class MainControl {
+
+
+    public static void main (String[] args) {
+       // halloWelt();
+        new Person("David Gregory Glusmann", 16);
+        new Person("Yener Varli", 16);
+        new Person("Bilal El Hawari", 17);
+    }
+
+
+    private static void halloWelt() {
+        System.out.println("Hello World!");
+        System.out.print("Wählt David");
+        System.out.print ("David");
+        System.out.print("Wählt");
+        System.out.print("ASDASHDBABSUDHABSDJKBAKDJKAKJDBKHABSDKB");
+    }
+
+}
---
+++ src/Person.java
@@ -0,0 +1,21 @@
+public class Person {
+
+    // Attribute{
+    private String name;
+    private int age;
+
+    // Referenzen
+
+    // Konstruktor
+     public Person(String name, int age) {
+         this.name = name;
+     this.age = age;
+         halloSagen();
+     }
+
+    // Methoden
+    public void halloSagen(){
+            System.out.println("Hi, my name is " + name +  " and I am "  + age + " years old ");
+    }
+
+}
